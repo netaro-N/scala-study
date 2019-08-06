f4ede8c5d3eb2cb4ea1dc9232153867417c3dce1
netaro-N <takebbb1+github@gmail.com>
2019-08-06 19:23:02 +0900

    first commit

---
+++ SimpleSearch.scala
@@ -0,0 +1,7 @@
+object SimpleSearch extends App {
+  val text ="カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndexes = Seq()
+
+  println(s"出現場所： ${matchIndexes}")
+}
